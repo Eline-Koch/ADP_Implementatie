b36b744ea8cf2a9cef264edc5a6660d4cc66b4c5
connor newton <c.m.newton@outlook.com>
2024-11-18 20:42:47 +0100

    Stack datastructure

---
+++ src/main/java/org/adp_implementatie/HanStackMetArray.java
@@ -0,0 +1,52 @@
+package org.adp_implementatie;
+
+public class HanStackMetArray {
+    private final Object[] stack;
+    private int top;
+
+    public HanStackMetArray() {
+        top = -1; // or else the first element of the array is selected
+        int maxSize = 5;
+        stack = new Object[maxSize];
+    }
+
+    public void push(Object item) {
+        top++;
+        this.stack[top] = item;
+    }
+
+    public void pop() {
+        top--;
+        this.stack[top] = null;
+    }
+
+    public Object peek() {
+        return stack[top];
+    }
+
+    public int size() {
+        return top + 1;
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public static void main(String[] args) {
+        HanStackMetArray HanStackMetArray = new HanStackMetArray();
+
+        System.out.println(HanStackMetArray.isEmpty());
+        HanStackMetArray.push(1);
+        HanStackMetArray.push(2);
+
+        System.out.println(HanStackMetArray.peek()); // 2
+
+        HanStackMetArray.pop();
+        System.out.println(HanStackMetArray.peek()); // 1
+
+        HanStackMetArray.pop();
+        System.out.println(HanStackMetArray.peek()); // Geeft een error want is leeg. dus correct
+    }
+}
+
+
