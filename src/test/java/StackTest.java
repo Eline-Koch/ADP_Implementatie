407ae615b9f3d2b2ec292baec9e945e7c0173279
connor newton <c.m.newton@outlook.com>
2024-11-23 16:19:15 +0100

    Basic testing

--- src/test/java/StackTest.java
+++ src/test/java/StackTest.java
@@ -5,7 +5,7 @@ public class StackTest {
     public static void main(String[] args) {
         PerformanceBenchmark benchmark = new PerformanceBenchmark();
 
-        int[] stackSize = {100, 10000, 100000, 1000000};
+        int[] stackSize = {1000, 100000, 1000000, 1000000};
 
         HanStackMetArray stack = new HanStackMetArray(stackSize[0]);
         HanStackMetArray stack2 = new HanStackMetArray(stackSize[1]);
@@ -13,26 +13,29 @@ public class StackTest {
 
         benchmark.start();
         for (int i = 0; i < stackSize[0]; i++) {
-            benchmark.recordOperation();
             stack.push(i);
         }
         benchmark.stop();
-        System.out.println("operations stack 1: " + benchmark.getOperations());
+        System.out.println("Stack add met size: " + stackSize[0]);
         benchmark.printElapsedTime();
+        System.out.println();
 
         benchmark.start();
         for (int i = 0; i < stackSize[1]; i++) {
             stack2.push(i);
         }
         benchmark.stop();
+        System.out.println("Stack add met size: " + stackSize[1]);
         benchmark.printElapsedTime();
+        System.out.println();
 
         benchmark.start();
         for (int i = 0; i < stackSize[2]; i++) {
             stack3.push(i);
         }
         benchmark.stop();
-        System.out.println("stacl 3");
+        System.out.println("Stack add met size: " + stackSize[2]);
         benchmark.printElapsedTime();
+        System.out.println();
     }
 }
