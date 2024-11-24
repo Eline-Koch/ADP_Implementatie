e86c0e5553da070d0a5cdac5d6f03033b985549a
connor newton <c.m.newton@outlook.com>
2024-11-24 14:04:31 +0100

    Basic testing

--- src/test/java/StackTest.java
+++ src/test/java/StackTest.java
@@ -3,39 +3,79 @@ import org.adp_implementatie.PerformanceBenchmark;
 
 public class StackTest {
     public static void main(String[] args) {
+        Pizza pizza = new Pizza("Doner kebab", false);
+
+        int[] stackSize = {1000, 10000, 100000, 1000000, 10000000, 100000000};
+
+        for (int i : stackSize) {
+            testStackAddOperation(i, "String insert");
+        }
+
+        for (int i : stackSize) {
+            testStackAddOperation(i, pizza);
+        }
+
+        for (int i : stackSize) {
+            testStackAddOperation(i, 1);
+        }
+
+        for (int i : stackSize) {
+            testStackPopOperation(i, 1);
+        }
+
+        for (int i : stackSize) {
+            testStackPeekOperation(i, 1);
+        }
+    }
+
+    public static void testStackAddOperation(int stackSize, Object pushObject) {
+        HanStackMetArray stack = new HanStackMetArray(stackSize);
         PerformanceBenchmark benchmark = new PerformanceBenchmark();
 
-        int[] stackSize = {1000, 100000, 1000000, 1000000};
-
-        HanStackMetArray stack = new HanStackMetArray(stackSize[0]);
-        HanStackMetArray stack2 = new HanStackMetArray(stackSize[1]);
-        HanStackMetArray stack3 = new HanStackMetArray(stackSize[2]);
-
         benchmark.start();
-        for (int i = 0; i < stackSize[0]; i++) {
-            stack.push(i);
+        for (int i = 0; i < stackSize; i++) {
+            stack.push(pushObject);
         }
         benchmark.stop();
-        System.out.println("Stack add met size: " + stackSize[0]);
+        System.out.println("Stack add met size: " + stackSize + " Object to insert: " + pushObject.toString());
         benchmark.printElapsedTime();
         System.out.println();
 
-        benchmark.start();
-        for (int i = 0; i < stackSize[1]; i++) {
-            stack2.push(i);
+    }
+
+    public static void testStackPopOperation(int stackSize, Object pushObject) {
+        HanStackMetArray stack = new HanStackMetArray(stackSize);
+        PerformanceBenchmark benchmark = new PerformanceBenchmark();
+
+        for (int i = 0; i < stackSize; i++) {
+            stack.push(pushObject);
         }
-        benchmark.stop();
-        System.out.println("Stack add met size: " + stackSize[1]);
-        benchmark.printElapsedTime();
-        System.out.println();
 
         benchmark.start();
-        for (int i = 0; i < stackSize[2]; i++) {
-            stack3.push(i);
+        for (int i = 0; i < stackSize - 1; i++) {
+            stack.pop();
         }
         benchmark.stop();
-        System.out.println("Stack add met size: " + stackSize[2]);
+        System.out.println("Stack Pop met size: " + stackSize + " Object to delete: " + pushObject.toString());
         benchmark.printElapsedTime();
         System.out.println();
+    }
+
+    public static void testStackPeekOperation(int stackSize, Object pushObject) {
+        HanStackMetArray stack = new HanStackMetArray(stackSize);
+        PerformanceBenchmark benchmark = new PerformanceBenchmark();
+
+        for (int i = 0; i < stackSize; i++) {
+            stack.push(pushObject);
+
+            if (i == stackSize / 2){
+                benchmark.start();
+                stack.peek();
+                benchmark.stop();
+                System.out.println("Stack Peek met size: " + stackSize + " Object to peek: " + pushObject.toString());
+                benchmark.printElapsedTime();
+                System.out.println();
+            }
+        }
     }
 }
